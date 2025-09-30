dd21c2666558994317f6984cf2587967fe04e141
Srikanth <srikanthgoudnarsng@gmail.com>
2025-09-30 17:16:31 +0530

    file updated by dev1

--- hello.java
+++ hello.java
@@ -8,6 +8,6 @@ class hello()   #from dev1
 	public static void main()    #from dev1
 >>>>>>> 7d8215a462369e7acce22515dd4ab4e9ed0b664b
 	{
-		system.out.println("hello from dev1");
+		system.out.println("hello from dev1 update");
 	
  #from dev2
